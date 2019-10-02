14656a46ea4c58d8033eedabddb9e0e852ccbd57
Ryan Newman <Ryan.N.Newman@Outlook.com>
2019-10-02 10:01:24 -0400

    Initial commit TimeKeepr

---
+++ TimeKeepr/TimeKeepr/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeKeepr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
