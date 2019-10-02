194f20e2a2e65ea56387ae86adc5971b9ce3e61c
LUke <porterl2268@student.faytechcc.edu>
2019-10-02 11:06:58 -0400

    added hardcoded function call

---
+++ TimeKeepr/TimeKeepr/Models/AddAdmin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeKeepr.Data;
+
+namespace TimeKeepr.Models
+{
+    public class AddAdmin
+    {
+        public static void EnsurePopulated(ApplicationDbContext context)
+        {
+            ;
+        }
+    }
+}
